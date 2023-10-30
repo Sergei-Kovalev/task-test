c8bfa777aba694289f095a0a878155963a16dd3c
Sergey Kovalev <s.kovalev.jdev@gmail.com>
2023-10-30 17:14:36 +0300

    Just start.

---
+++ src/test/java/ru/clevertec/product/mapper/impl/ProductMapperImplTest.java
@@ -0,0 +1,18 @@
+package ru.clevertec.product.mapper.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductMapperImplTest {
+
+    @Test
+    void toProduct() {
+        // given
+
+
+        // when
+
+        // then
+    }
+}
